796460193cfd9f0b9090617aa2662692a97b8888
Minh Van Nguyen <van23112002@gmail.com>
2022-03-03 16:35:04 +0700

    next 3 scr

--- pages/navbar.js
+++ pages/navbar.js
@@ -19,19 +19,26 @@ export default function Navbar() {
         <>
             <nav className="bg-transparent border-gray-200 px-2 sm:px-4 py-2.5 rounded">
                 <div className="container flex flex-wrap justify-between items-center mx-auto">
-                    <a href="/" className="flex items-center">
-                        <Image src="/images/Logo.svg" height={45} width={145} />
-                    </a>
+                    <Link href='/'>
+                        <a className="flex items-center">
+                            <Image src="/images/Logo.svg" height={45} width={145} />
+                        </a>
+                    </Link>
+                    
                     <div className="w-auto inline-block" id="mobile-menu">
                         <ul className="flex flex-col pt-4 md:flex-row md:items-center md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                             <li className={MainStyles.lighttext}>
-                                <a href="/" className="block py-2 pr-4 pl-3 rounded md:bg-transparent md:p-0" aria-current="page">Body Type Assessment</a>
+                            <Link href='/'>
+                                <a  className="block py-2 pr-4 pl-3 rounded md:bg-transparent md:p-0" aria-current="page">Body Type Assessment</a>
+                                </Link>
                             </li>
-                            <li className={MainStyles.lighttext}>
-                                <a href="/" className="block py-2 pr-4 pl-3 rounded md:bg-transparent md:p-0" aria-current="page">Body Recovery Assessmemt</a>
+                            <li className={MainStyles.lighttext}><Link href='/'>
+                                <a className="block py-2 pr-4 pl-3 rounded md:bg-transparent md:p-0" aria-current="page">Body Recovery Assessmemt</a>
+                                </Link>
                             </li>
-                            <li className={MainStyles.lighttext}>
-                                <a href="/" className="block py-2 pr-4 pl-3 rounded md:bg-transparent md:p-0" aria-current="page">Wellbeing Toolkit</a>
+                            <li className={MainStyles.lighttext}><Link href='/'>
+                                <a className="block py-2 pr-4 pl-3 rounded md:bg-transparent md:p-0" aria-current="page">Wellbeing Toolkit</a>
+                                </Link>
                             </li>
                             <li>
                                 <button data-collapse-toggle="mobile-menu" type="button" className="inline-block" aria-controls="mobile-menu-2" aria-expanded="false">
