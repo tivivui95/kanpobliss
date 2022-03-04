21953046203d88ea8887e820679c0d59606c6f05
Minh Van Nguyen <van23112002@gmail.com>
2022-03-04 10:07:48 +0700

    next 1 scr

--- pages/main.js
+++ pages/main.js
@@ -2,15 +2,37 @@ import Navbar, { MBFooter } from "./navbar";
 import MainStyles from '../styles/Main.module.css'
 import Image from "next/image";
 
+const FirstContent = () => {
+    return (
+        <>
+            <div>
+                <Image src='/images/4/Body Type.png' alt='' width={100} height={100} />
+            </div>
+            
+        </>
+    )
+}
+
+function MainImageSlider() {
+    return (
+        <>
+        <div className="">
+            <FirstContent />
+        </div>
+        </>
+    )
+}
+
 function Content() {
     return (
         <>
         <div className="ml-12 mt-6">
             <Image src='/images/image 14.png' className={MainStyles.moonimg} width={148} height={148} />
-            <div className={MainStyles.head1}>Hello, friend!</div>
+            <div className={MainStyles.head1}>Hello, &nbsp;<h1>friend!</h1></div>
             <div className={MainStyles.head2}>How would you like to get started?</div>
             <div className={MainStyles.space}></div>
             <div className={MainStyles.head2}>Choose from one of the following</div>
+            <MainImageSlider />
         </div>
             
         </>
