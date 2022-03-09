005fd92f4e53a67e8d0db46d064e40d04b173cc6
Minh Van Nguyen <van23112002@gmail.com>
2022-03-10 01:42:13 +0700

    scr 15, 16, 17 and fix 11 12 13 14

--- pages/16.js
+++ pages/16.js
@@ -24,7 +24,7 @@ const Content16 = ({ Mantra }) => {
                 </div>
                 <p className={styles.paragraph}>You can enjoy all kinds of food in moderation! Opt for nutrient-filled vegetables, good quality meat, some good fats. Limit sugar for occasional treats only! We recommend eating well 80% of the time and leave the 20% for indulgence foods like pasta or glazed donuts.  
                     <br/>
-                    Superfoods and trendy food doesn't mean that it can be taken in large quantities every single day. A little a day will suffice... <div className={styles.readmore} id="readmore">Learn More</div> </p>
+                    Superfoods and trendy food doesn&apos;t mean that it can be taken in large quantities every single day. A little a day will suffice... <div className={styles.readmore} id="readmore">Learn More</div> </p>
             </div>
             <div className={styles.block}>
                 <div className={styles.heading}>
@@ -32,7 +32,7 @@ const Content16 = ({ Mantra }) => {
                     <Image src="/images/16/LifeStyle.png" width={23.65} height={25.17} alt="" />
                     <h3 className={styles.header}>&nbsp; Lifestyle & habits</h3>
                 </div>
-                <p className={styles.paragraph}>Go for a walk after dinner instead of binging on Netflix. Opt for LIIT (Light Intensity Interval Training) when you're tired. Join that HIIT class you've always wanted to try. 
+                <p className={styles.paragraph}>Go for a walk after dinner instead of binging on Netflix. Opt for LIIT (Light Intensity Interval Training) when you&apos;re tired. Join that HIIT class you&apos;ve always wanted to try. 
                     <br/>
                     Be in touch with your energy level and do the movements you feel like. Exercise should make you refreshed (and a little tired!) but not exhausted... <div className={styles.readmore} id="readmore">Learn More</div> </p>
             </div>
--- pages/17.js
+++ pages/17.js
@@ -10,29 +10,29 @@ const Content17 = ({ Name }) => {
         <Navbar />
         <div className={styles.contain}>
             
-        <div class={styles.box}>
-            <div class={styles.heading}>
-                {/* <img class={styles.logo" src="assets17/logo.png}> */}
+        <div className={styles.box}>
+            <div className={styles.heading}>
+                {/* <img className={styles.logo" src="assets17/logo.png}> */}
                 <Image src="/images/17/logo.png" width={140} height={140} objectFit="scale-down" alt="" />
-                <div class={styles.infor}>
-                    <h1 class={styles.hello}>Hello, {Name}!</h1>
-                    <h1 class={styles.bio}>Here're the recomnendations for you to live your wellness mantra</h1>
+                <div className={styles.infor}>
+                    <h1 className={styles.hello}>Hello, {Name}!</h1>
+                    <h1 className={styles.bio}>Here're the recomnendations for you to live your wellness mantra</h1>
                 </div>
             </div>
-            <div class={styles.choose}>Choose from one of the following</div>
-            <div class={styles.container}>
-                {/* <img src="assets17/image 7.png" alt="" class={styles.image}>
-                <img src="assets17/image 8.png" alt="" class={styles.image}>
-                <img src="assets17/image 6.png" alt="" class={styles.image}>
-                <img src="assets17/image 5.png" alt="" class={styles.image}> */}
+            <div className={styles.choose}>Choose from one of the following</div>
+            <div className={styles.container}>
+                {/* <img src="assets17/image 7.png" alt="" className={styles.image}>
+                <img src="assets17/image 8.png" alt="" className={styles.image}>
+                <img src="assets17/image 6.png" alt="" className={styles.image}>
+                <img src="assets17/image 5.png" alt="" className={styles.image}> */}
                 <Image src="/images/17/image 7.png" width={360} height={405} alt="" objectFit="scale-down" />
                 <Image src="/images/17/image 8.png" width={360} height={405} alt="" objectFit="scale-down" />
                 <Image src="/images/17/image 6.png" width={360} height={405} alt="" objectFit="scale-down" />
                 <Image src="/images/17/image 5.png" width={360} height={405} alt="" objectFit="scale-down" />
             </div>
-            <div class={styles.slider}>
-                <div class={styles.green}></div>
-                <div class={styles.white}></div>
+            <div className={styles.slider}>
+                <div className={styles.green}></div>
+                <div className={styles.white}></div>
             </div>
         </div>
         </div>
