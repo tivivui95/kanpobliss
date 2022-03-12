8dd2f20377baabd6e360b7371d36f0d72808fcbe
Minh Van Nguyen <van23112002@gmail.com>
2022-03-13 02:14:46 +0700

    fixed bug

--- components/socialLogin.js
+++ components/socialLogin.js
@@ -8,13 +8,13 @@ export default function SocialLogin() {
     return (
         <>
         <div className={styles.footer}>
-                        <div>or Sign Up Using</div>
+                        <div className="nearwhite">or Sign Up Using</div>
                         <div className={styles.socialSpan}>
                             <Image src="/images/11/Facebook.png" alt='' width={46} height={46} />
                             <Image src="/images/11/Twitter.png" alt=''  width={46} height={46} />
                             <Image src="/images/11/Gmail.png" alt='' width={46} height={46} />
                         </div>
-                        <div>Already has an account? <a className={styles.a}>Sign In</a></div>
+                        <div className="nearwhite">Already has an account? <a className={styles.a}>Sign In</a></div>
                     </div>
         </>
     )
