bfaeb756a4675114d942e2c5394da7d1e5d04e6f
Minh Van Nguyen <van23112002@gmail.com>
2022-03-15 22:01:20 +0700

    screen 28 fixed 3

--- pages/28.js
+++ pages/28.js
@@ -94,7 +94,7 @@ export default function Content25() {
             <div className={styles.coverimg}>
                 <div className={styles.contentcover}>
                     <div className='container mx-auto p-4 md:pt-12 pt-8'>
-                    <div className='text-center text-base md:text-2xl'>
+                    <div className='text-center text-base md:text-xl'>
                     I am self taught in TCM and this app is very useful for narrowing down issues in the body. The explanations are clear enough that someone who doesn't have experience/knowledge of TCM can use it to gain insight and be able to heal their body with lifestyle and diet changes.
                     </div>
                     <div className='relative h-2 w-full mt-2 md:mt-6 mb-2 md:mb-6'>
