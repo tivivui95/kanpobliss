cecd57b1e9ba424eceb279308aba483c71c557db
Minh Van Nguyen <van23112002@gmail.com>
2022-03-20 20:28:13 +0700

    config quiz complete

--- pages/test.js
+++ pages/test.js
@@ -1,35 +1,34 @@
-import App from '../components/quiz/dragItems'
-import React, { useState, useEffect } from 'react';
+import React, {useEffect, useState} from "react";
+import EmblaCarousel from "../components/EmblaCarousel";
+// import Footer from "./Footer";
+// import "../css/base.css";
+// import "../css/reset.css";
 
-const data = [
-    {
-        content: 'Poor appetite',
-        id: 'item-1'
-    },
-    {
-        content: 'Frequent bloating',
-        id: 'item-2',
-    },
-    {
-        content: 'Prefer hot food and drinks',
-        id: 'item-3',
-    }
-];
-
-export default function Demo() {
-    const [winReady, setwinReady] = useState(false);
-
-    const onSubmitBtn = (datalist) => {
-        console.log(datalist);
-    }
-
+function App() {
+    const [ winCheck, onWinCheck ] = useState(false);
+    
     useEffect(() => {
-        setwinReady(true);
+        onWinCheck(true);
     }, []);
+
     return (
-        <div className="pl-4 pr-4 pt-3">
-           ` <h5>Frontend Board</h5>`
-            {winReady ? <App allItems={data} onSub={onSubmitBtn} /> : null}
-        </div>
-    );
-} 
+        <>
+    {winCheck ? <div className="content">
+      <EmblaCarousel autoplay delayLength={2000}>
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+      </EmblaCarousel>
+    </div>: null}
+    
+    {/* <Footer /> */}
+  </>
+    )
+  
+};
+
+export default App;
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
