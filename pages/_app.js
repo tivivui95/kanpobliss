bfd17f3228a0a3fe1cc91e1976eabd87d2f7718f
Minh Van Nguyen <van23112002@gmail.com>
2022-03-23 00:27:44 +0700

    final flow quiz 1 (fixed)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
+// import "bootstrap-icons/font/bootstrap-icons.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
