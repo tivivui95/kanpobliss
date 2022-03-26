7d4390af01ff91939453f9a621480d909acfe18f
vantutrieu97 <vantutrieu97@gmail.com>
2022-03-26 18:17:01 +0700

    Removed code unused

--- pages/quiz.js
+++ pages/quiz.js
@@ -6,8 +6,11 @@ import Content10 from "./views/10";
 import {Allquestion, MantraList} from '../components/quiz/quiz1data';
 import { RenderQuestion, RenderBonus } from "../components/quiz/QuesList";
 import App from '../components/quiz/dragItems';
+import {Button, Modal, ModalBody, ModalHeader} from "reactstrap";
+import RequestChooseAnswerModal from "../components/quiz";
+import ModalExample from "../components/quiz";
 
-export default function Main() {
+export default ()=> {
     const [curPage, onChangePage] = useState(0);
     const [result, onChangeResult] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
     const [showItem, onChangeShow] = useState(false);
@@ -20,11 +23,14 @@ export default function Main() {
 
     const [ resultTie, onResultTie ] = useState(false);
 
+
+    const [isShowDialog, setShowDialog] = useState(false);
+
     useEffect(() => {
         setwinReady(true);
     }, []);
 
-    const onSubmit = (lastresult) => {        
+    const onSubmit = () => {
         let resSorted = [];
         result.map((el, indx) => indx < 11 ? resSorted.push(el): null);
         resSorted[0] = 0;
@@ -127,6 +133,10 @@ export default function Main() {
     }
 
     const handleNextPage = (resArr, ansList) => {
+        console.log('-------------------------------------------------------------------');
+        console.log(resArr);
+        console.log(ansList);
+        console.log('-------------------------------------------------------------------');
         if (resArr.reduce((a, b) => a + b, 0) > 0) {
             curPage < 9 ? onChangePage(curPage + 1) : onChangePage(curPage);
             if (curPage < 9) {
@@ -139,8 +149,13 @@ export default function Main() {
                 onChangeResult(new_arr);
                 onChangeAnsLists(temp_ans_list);
             }
+        }else{
+            curPage == 9 ? onSubmit(): showDialog();
         }
-        curPage == 9 ? onSubmit(): null;
+    }
+    const showDialog=() =>{
+        console.log('show dialog nè',isShowDialog);
+        setShowDialog(!isShowDialog);
     }
 
     const handlePrevPage = (resArr, ansList) => {
@@ -166,9 +181,9 @@ export default function Main() {
                 {showMantra ? <div><Content10 Mantra={MantraList[mantra]} /></div> : <div className={!showMantra ? '' : 'hidden'}>
                 <div className="container mx-auto md:mt-8 mb-8 grid justify-items-center relative ">
                     <div className={styles.contain}>
-                        
+
                         <div className="container mx-auto">
-                            {resultTie ? <RenderBonus data={dragl} onSub={onSecondSubmit} />: 
+                            {resultTie ? <RenderBonus data={dragl} onSub={onSecondSubmit} />:
                             <>
                             <div className={showItem && !showMantra ? 'block' : 'hidden'}>
                             <div className={styles.head2}>Drag & re-order the symptoms you selected previously. </div>
@@ -180,12 +195,12 @@ export default function Main() {
                                     <div className={curPage == index ? 'block' : 'hidden'} key={index * 100}>
                                         <RenderQuestion questionJs={content} handlePrev={handlePrevPage} handleNext={handleNextPage} />
                                     </div>
-                                ))}                                
-                                
+                                ))}
+
                             </div>
                             </>
                             }
-                            
+
                         </div>
                     </div>
                     <div className="mt-4"></div>
@@ -196,14 +211,10 @@ export default function Main() {
                         </div>
                     </div>
                 </div>
-
-
                 <MBFooter />
                 <Footer />
                 </div>}
-                
-                
-            </div>
+                </div>
         </>
     )
 }
